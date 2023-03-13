36dd2b1b72ca617f7a32cf56d83b02934c54b09a
Markos Girgis <mg.markosgirgis@gmail.com>
2023-03-13 17:27:51 +0200

    use repo ports

--- src/heroes/application/find-all-heroes/find-all-heroes.handler.ts
+++ src/heroes/application/find-all-heroes/find-all-heroes.handler.ts
@@ -1,15 +1,15 @@
 import { IQueryHandler, QueryHandler } from '@nestjs/cqrs';
 import { KillDragonCommand } from '../kill-dragon/kill-dragon.command';
-import { HeroRepository } from '../../infra/repository/hero.repository';
 import { Inject } from '@nestjs/common';
 import { HEROES_REPOSITORY } from '../../heroes.di-tokens';
 import { Hero } from '../../domain/hero.entity';
+import { HeroRepositoryPort } from 'src/heroes/infra/repository/hero.repository.port';
 
 @QueryHandler(QueryHandler)
 export class FindAllHeroesHandler implements IQueryHandler<KillDragonCommand> {
   constructor(
     @Inject(HEROES_REPOSITORY)
-    private repository: HeroRepository,
+    private repository: HeroRepositoryPort,
   ) {}
 
   async execute(): Promise<Hero[]> {

3f84c42043d774e2e5ae40dd37d03fb87c4b59e1
ellik95 <ellikotoula@gmail.com>
2023-03-22 10:40:59 +0200

    fixed login validator to check about the email too

--- src/infra/auth/local-auth.guard.ts
+++ src/infra/auth/local-auth.guard.ts
@@ -1,5 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { LoginDTO } from '@src/api/dto/login.dto';
+import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest<Request>();
+    const loginDto = plainToClass(LoginDTO, req.body);
+    const errors = await validate(loginDto);
+
+    if (errors.length > 0) {
+      throw new BadRequestException(errors);
+    }
+
+    return super.canActivate(context) as Promise<boolean>;
+  }
+}
